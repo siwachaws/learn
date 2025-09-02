203c48377789cf9c70e6ea5bde5cab4578f26f01
Himanshu <himanshu.siwach@email.com>
2025-09-02 21:10:56 +0530

    done review changes

--- PLSQL_Practice.sql
+++ PLSQL_Practice.sql
@@ -9,5 +9,5 @@ BEGIN
     r_emp.salary:= r_emp.salary*v_salary_increase + r_emp.salary * nvl(r_emp.commission_pct,0);
     update employees_copy set row = r_emp where current of c_emps;
     DBMS_OUTPUT.PUT_LINE('The salary of : ' || r_emp.employee_id || ' is increased from '|| v_old_salary || ' to ' || r_emp_salary);
-    end loop;
-end;
+    END LOOP;
+END;
